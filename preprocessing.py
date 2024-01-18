f352e65750b3acca51403a47b4ee322392e6f61a
ardimou <124301215+ardimou@users.noreply.github.com>
2024-01-18 11:59:06 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -176,26 +176,31 @@ def create_dataset(folder_dir, anns_file, save_folder):
                 pickle.dump(data, file)
             file_name += 1
 
-def visualize_samples(folder, item):
-    with open(f'{folder}/{item}.pkl', 'rb') as file:
-      data = pickle.load(file)
-    
-    plt.imshow(data['S_t1'])
-    plt.title('S_t')
-    plt.show()   
-    
-    plt.imshow(data['target_t1'])
-    plt.title('M_t')
-    plt.show()
-    
-    plt.imshow(data['S_t2'])
-    plt.title('S_t+1')
-    plt.show()   
-    
-    plt.imshow(data['target_t2'])
-    plt.title('M_t+1')
-    plt.show()
-    
+ def visualize_samples(folder, item):
+        with open(f'{folder}/{item}.pkl', 'rb') as file:
+          data = pickle.load(file)
+        push = data['push']
+        
+        plt.imshow(data['S_t1'])
+        plt.title('S_t')
+        plt.plot(push[:,0], push[:,1], 'r')
+        plt.scatter(push[1,0], push[1,1], c='k')
+        plt.show()
+        
+        plt.imshow(data['target_t1'])
+        plt.title('M_t')
+        plt.show()
+        
+        plt.imshow(data['S_t2'])
+        plt.title('S_t+1')
+        plt.plot(push[:,0], push[:,1], 'r')
+        plt.scatter(push[1,0], push[1,1], c='k')
+        plt.show() 
+         
+        
+        plt.imshow(data['target_t2'])
+        plt.title('M_t+1')
+        plt.show()
     
     
     
